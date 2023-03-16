c54e6d3b30b5e9a308de058ebd92fa78be615321
Burak Atilla <burak.atilla@gmail.com>
2023-03-16 10:16:56 +0300

    static files upload

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-const deneme = () => {
-  console.log('deneme txtr');
-};
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
 
-deneme();
+app.use(express.static('public'));
+
+//index
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
+
+// listen function
+app.listen(port, () => {
+  console.log(`server is up and running on port : ${port}`);
+});
